f820ff446d583724b295ed9941b280e9dd618c2f
shivankdixit111 <dshivank630@gmail.com>
2024-10-30 10:02:13 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} **/
 const nextConfig = { 
-    images: {
-        loader: 'custom',
-        loaderFile: './my-loader.ts',
-    },
+    
     images:{
         domains: ['images.unsplash.com',"/courses"]
     }
