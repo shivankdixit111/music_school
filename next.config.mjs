5e6c7666489e8c234534d810c3aa291155e546aa
shivankdixit111 <dshivank630@gmail.com>
2024-10-30 10:19:59 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} **/
 const nextConfig = { 
     
-    images:{
-        domains: ['images.unsplash.com'],
+    images:{ 
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'images.unsplash.com',
+              port: '',
+              pathname: '/**',
+            },
+        ]
     }
 };
 
